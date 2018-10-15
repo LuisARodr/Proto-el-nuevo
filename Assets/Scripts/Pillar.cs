4768eb462deeccc782ce674b784e2b2939cc2ff6
TheMajesticBox <42950184+TheMajesticBox@users.noreply.github.com>
2018-10-15 03:16:26 -0700

    PuesTodoLoQueHizoLaCaja
    Leer el titulo

---
+++ Assets/Scripts/Pillar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillar : MonoBehaviour {
+
+    [SerializeField]
+    int golpes = 3; float TiempoParpadeo = .5f;
+    [SerializeField]
+    Material mat1,  mat2;
+ 
+    bool coll = false;
+    Material mat;
+    MeshRenderer rend;
+    private void Awake()
+    {
+        rend = GetComponent<MeshRenderer>();
+    }
+    private void Update()
+    {
+        if(TiempoParpadeo > 0 && coll)
+        {
+            TiempoParpadeo -= Time.deltaTime;
+        }
+        if(TiempoParpadeo  <0.01f)
+        {
+            TiempoParpadeo = .5f;
+            coll = false;
+            rend.material = mat1;
+        } 
+        if(golpes <= 0)
+        {
+            Destroy(this.gameObject);
+            golpes = 3;
+        }
+    }
+        private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Bullet(Clone)")
+        {
+            Debug.Log("eNTRO la bala");
+            
+            rend.material = mat2;
+            coll = true;
+            golpes--;
+        }
+
+    }      
+}
---
+++ Assets/Scripts/Plataform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataform : MonoBehaviour {
+
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entro m");
+        if (other.gameObject.name == "StandingCollider")
+        {
+            Player.transform.parent = transform;
+            Debug.Log("Entro");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Entro s");
+        if (other.gameObject.name == "StandingCollider")
+        {
+            Player.transform.parent = null;
+            Debug.Log("Salio");
+        }
+    }
+
+
+
+}
